e71d3ffc7e1cdaac740ef8d7ae46e79255e5b579
nathanpua <81512446+nathanpua@users.noreply.github.com>
2025-03-10 00:56:07 +0800

    dply

--- options.js
+++ options.js
@@ -65,7 +65,7 @@ const restoreOptions = () => {
   chrome.storage.local.get(
     {
       userId: '',
-      websiteUrl: 'https://techfest2025-five.vercel.app/' // Default website URL
+      websiteUrl: 'https://techfest2025-delta.vercel.app/' // Default website URL
     },
     (items) => {
       document.getElementById('userId').value = items.userId;
